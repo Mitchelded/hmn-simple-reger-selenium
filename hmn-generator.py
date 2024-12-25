731085e60eefa800df7f2f0da19a4b653c5e0421
Mitchelded <tretyakovanton0000@gmail.com>
2024-12-25 10:10:47 +0300

    change request to selenium

--- hmn-generator.py
+++ hmn-generator.py
@@ -1,52 +1,39 @@
-from os import system
-system('pip3 install fake-useragent requests')
-import requests
-from fake_useragent import UserAgent
+import time
+import google_colab_selenium as gs
 
 
-root_url = 'https://hidemy.io'
-demo_url = root_url + '/ru/demo/'
-ua = UserAgent()
-headers = {'User-Agent': ua.random}
-proxy = input('Введите прокси в формате `ip:port`. Оставьте поле пустым, чтобы не использовать прокси: ')
-if proxy == '':
-    proxies = None
-else:
-    proxies = {
-        'https': f'http://{proxy}',
-    }
+driver = gs.UndetectedChrome()
 
 try:
-    demo_page = requests.get(demo_url, headers=headers, proxies=proxies)
-except Exception as e:
-    print('Ошибка доступа к сайту! Проверьте правильность прокси или попробуйте другой!')
-
-if 'Ваша электронная почта' in demo_page.text:
-    
-    email = input('Введите электронную почту для получения тестового периода: ')
-
-    response = requests.post('https://hidemy.io/ru/demo/success/', data={
-        "demo_mail": f"{email}"
-    }, headers=headers, proxies=proxies)
-
-    if 'Ваш код выслан на почту' in response.text:
-        confirm = input('Введите полученную ссылку для подтверждения e-mail адреса: ')
-        
-        while True:
-            try:
-                response = requests.get(confirm, headers=headers, proxies=proxies)
-                if 'Спасибо' in response.text:
-                    print('Почта подтверждена. Код отправлен на вашу почту!')
-                    break
-                else:
+    demo_url = 'https://hidemy.io/ru/demo/'
+    driver.get(demo_url)
+    time.sleep(3)  # Дождаться загрузки страницы
+    if "Ваша электронная почта" in driver.page_source:
+        email = input('Введите электронную почту для получения тестового периода: ')
+        email_input = driver.find_element(By.NAME, "demo_mail")
+        email_input.send_keys(email)
+        email_input.send_keys(Keys.RETURN)
+        time.sleep(3)
+        if "Ваш код выслан на почту" in driver.page_source:
+            print("Код отправлен на почту. Подтвердите e-mail.")
+            confirm = input('Введите ссылку для подтверждения e-mail адреса: ')
+            while True:
+                try:
+                    driver.get(confirm)
+                    time.sleep(3)
+                    if "Спасибо" in driver.page_source:
+                        print("Почта подтверждена. Код отправлен на вашу почту!")
+                        break
+                    else:
+                        confirm = input('Ссылка невалидная, повторите попытку: ')
+                except Exception as e:
+                    print(f"Ошибка подтверждения ссылки: {e}")
                     confirm = input('Ссылка невалидная, повторите попытку: ')
-            except:
-                confirm = input('Ссылка невалидная, повторите попытку: ')
-                continue
-
-
+        else:
+            print("Указанная почта не подходит для получения тестового периода.")
     else:
-        print('Указанная почта не подходит для получения тестового периода ')
-
-else:
-    print('Невозможно получить тестовый период')
+        print("Форма ввода почты не найдена на странице.")
+except Exception as e:
+    print(f"Произошла ошибка: {e}")
+finally:
+    driver.quit()
